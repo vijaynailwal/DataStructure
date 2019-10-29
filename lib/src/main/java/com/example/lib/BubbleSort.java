c12733dbb7b9c0cd8fd58912707937dffb349e40
vijaynailwal1992 <vijaynailwal92@gmail.com>
2019-10-29 16:33:56 +0530

    remove comments

--- lib/src/main/java/com/example/lib/BubbleSort.java
+++ lib/src/main/java/com/example/lib/BubbleSort.java
@@ -4,14 +4,13 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int arr[] = {20, 10, 50, 40};
-        bubbleSort(arr);//sorting array elements using bubble sort
-
+        bubbleSort(arr);
     }
 
     static void bubbleSort(int[] array) {
         int n = array.length;
         int temp = 0;
-        for (int i = 0; i < n; i++) // Looping through the array length
+        for (int i = 0; i < n; i++)
         {
             for (int j = 1; j < (n - i); j++) {
                 System.out.println("Comparing " + array[j - 1] + " and " + array[j]);
@@ -21,9 +20,6 @@ public class BubbleSort {
                     array[j - 1] = array[j];
                     array[j] = temp;
                     printArray(array);
-
-
-
 
                 }
             }
